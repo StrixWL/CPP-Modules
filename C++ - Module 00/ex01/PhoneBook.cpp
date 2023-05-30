9a63bf08c0e68a3bec32f60aa2b4724e1e469958
Strix <brahimstrix@gmail.com>
2023-05-30 05:55:24 +0100

    Module 00: ex01 (updated phoneBook display)

--- C++ - Module 00/ex01/PhoneBook.cpp
+++ C++ - Module 00/ex01/PhoneBook.cpp
@@ -7,7 +7,7 @@ PhoneBook::PhoneBook(void) {
 }
 
 void PhoneBook::add(Contact newContact) {
-	contacts[index] = newContact;
+	*(contacts + index) = newContact;
 	index = ++index % 8;
 	if (!index)
 		filled = true;
@@ -18,7 +18,15 @@ Contact *PhoneBook::getContacts(void) {
 }
 
 void	PhoneBook::display(void) {
-	for (int i = 0, k = (filled ? 8 : index), j = index; i < k; i++, j++) {
-		std::cout << contacts[j % k].getData(nickname) << std::endl;
+	for (int i = 0, k = filled ? 8 : index, j = index; i < k; i++, j++) {
+		Contact *contact = contacts + j % k;
+		std::string
+			_firstName = contact->getData(firstName),
+			_lastName = contact->getData(lastName),
+			_nickname = contact->getData(nickname);
+		std::cout << "|         " << i;
+		std::cout << "|" << std::string("          ").erase(10 - _firstName.substr(0, 10).size()) << _firstName.substr(0, 9) << ((_firstName.size() > 9) ? "." : "");
+		std::cout << "|" << std::string("          ").erase(10 - _lastName.substr(0, 10).size()) << _lastName.substr(0, 9) << ((_lastName.size() > 9) ? "." : "");
+		std::cout << "|" << std::string("          ").erase(10 - _nickname.substr(0, 10).size()) << _nickname.substr(0, 9) << ((_nickname.size() > 9) ? "." : "") << "|" <<  std::endl;
 	}
 }
--- C++ - Module 00/ex01/main.cpp
+++ C++ - Module 00/ex01/main.cpp
@@ -5,19 +5,19 @@
 int main(void) {
 	//first name, last name, nickname, phone number, and darkest secret
 	PhoneBook myBook;
-	myBook.add(Contact("Brahim", "El Amri", "Strix0", "0611212432", "hates everyoen"));
-	myBook.add(Contact("Brahim", "El Amri", "Strix1", "0611212432", "hates everyoen"));
-	myBook.add(Contact("Brahim", "El Amri", "Strix2", "0611212432", "hates everyoen"));
-	myBook.add(Contact("Brahim", "El Amri", "Strix3", "0611212432", "hates everyoen"));
-	myBook.add(Contact("Brahim", "El Amri", "Strix4", "0611212432", "hates everyoen"));
-	myBook.add(Contact("Brahim", "El Amri", "Strix5", "0611212432", "hates everyoen"));
-	myBook.add(Contact("Brahim", "El Amri", "Strix6", "0611212432", "hates everyoen"));
-	myBook.add(Contact("Brahim", "El Amri", "Strix7", "0611212432", "hates everyoen"));
-	myBook.add(Contact("Brahim", "El Amri", "Strix8", "0611212432", "hates everyoen"));
-	myBook.add(Contact("Brahim", "El Amri", "Strix9", "0611212432", "hates everyoen"));
-	myBook.add(Contact("Brahim", "El Amri", "Strix10", "0611212432", "hates everyoen"));
-	myBook.add(Contact("Brahim", "El Amri", "Strix11", "0611212432", "hates everyoen"));
-	myBook.add(Contact("Brahim", "El Amri", "Strix12", "0611212432", "hates everyoen"));
-	myBook.add(Contact("Brahim", "El Amri", "Strix13", "0611212432", "hates everyoen"));
+	myBook.add(Contact("Brahim0", "El Amri0", "Strix0", "0611212432", "hates everyoen"));
+	myBook.add(Contact("Brahim1", "El Amri1", "Strix1", "0611212432", "hates everyoen"));
+	myBook.add(Contact("Brahim2", "El Amri2", "Strix2", "0611212432", "hates everyoen"));
+	myBook.add(Contact("Brahim3", "El Amri3", "Strix3", "0611212432", "hates everyoen"));
+	myBook.add(Contact("Brahim4", "El Amri4", "Strixewqdeqsad4", "0611212432", "hates everyoen"));
+	myBook.add(Contact("Brahim5", "El Amri5", "Strix5", "0611212432", "hates everyoen"));
+	myBook.add(Contact("Brahim6", "El Amrieqweqw6", "Strix6", "0611212432", "hates everyoen"));
+	myBook.add(Contact("Brahim7", "El Amri7", "Strix7", "0611212432", "hates everyoen"));
+	myBook.add(Contact("Brahim8", "El Amri8", "Strix8", "0611212432", "hates everyoen"));
+	myBook.add(Contact("Brahim9", "El Amri9", "Strix9", "0611212432", "hates everyoen"));
+	myBook.add(Contact("Brahimewqeqweqweqweqw10", "El Amri10", "Strix10", "0611212432", "hates everyoen"));
+	myBook.add(Contact("Brahim11", "El Amri11", "Strix11", "0611212432", "hates everyoen"));
+	myBook.add(Contact("Brahim12", "El Amri12", "Strix12", "0611212432", "hates everyoen"));
+	myBook.add(Contact("Brahim13", "El Amri13", "Strix13", "0611212432", "hates everyoen"));
 	myBook.display();
 }
